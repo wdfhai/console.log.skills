8e57e88031f1c730e1698af0becf7c50f94b3f75
wdfhai <wd.fhai@outlook.com>
2020-12-11 17:59:47 -0600

    added questions and answers arrays

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var questions = [
+    "Which of the following is not an element in HTML?",
+    "In CSS, IDs are linked using the following syntax...",
+    "Which one of the following is a self-closing element?",
+    "Which one of the following is NOT a semantic tag in HTML?",
+    "In CSS, which is the outermost part of the box model?",
+    "Which of the following is an example of 'properties' in Javascript?",
+    "In Javascript, the Math.random function would NOT generate the following...",
+    "In the array ['fruits', 'vegetables', 'meats', 'breads'], what is the index of 'fruits'?",
+    "Which one of the following can NOT be used to store a variable in Javascript?",
+    "Which tag is used to write Javascript code inside of an HTML file?",
+]
+
+var answers = [
+    "position",
+    "#id",
+    "img",
+    "div",
+    "margin",
+    "firstName:John",
+    "1",
+    "0",
+    "make",
+    "<script>"
+]
