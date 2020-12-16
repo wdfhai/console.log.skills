0e50ed2202bb13c6f112c67de3f6191083091233
wdfhai <wd.fhai@outlook.com>
2020-12-15 22:07:36 -0600

    added the Fisher-Yates algorith, see comments

--- assets/script.js
+++ assets/script.js
@@ -9,39 +9,33 @@ var questions = [
     "In the array ['fruits', 'vegetables', 'meats', 'breads'], what is the index of 'fruits'?",
     "Which one of the following can NOT be used to store a variable in Javascript?",
     "Which tag is used to write Javascript code inside of an HTML file?",
-]
+];
 
-var answers = [
-    "position",
-    "#id",
-    "img",
-    "div",
-    "margin",
-    "firstName:John",
-    "1",
-    "0",
-    "make",
-    "script"
-]
-
-var incorrectOne = ["body", "header", "section"];
-var incorrectTwo = [".id", "$id", "&id"];
-var incorrectThree = ["div", "p", "h1"];
-var incorrectFour = ["section", "footer","header"];
-var incorrectFive = ["padding", "border", "content"];
-var incorrectSix = ["addEventListener", "getElementById", "array.join"];
-var incorrectSeven = ["0", "0.5", "0.99999"];
-var incorrectEight = ["1", "2", "1.1"];
-var incorrectNine = ["var", "let", "const"];
-var incorrectTen = ["style", "interact", "java"];
+var answersOne = ["position", "body", "header", "section"];
+var answersTwo = ["#id", ".id", "$id", "&id"];
+var answersThree = ["img", "div", "p", "h1"];
+var answersFour = ["div", "section", "footer","header"];
+var answersFive = ["padding", "margin", "border", "content"];
+var answersSix = ["addEventListener", "getElementById", "firstName:John", "array.join"];
+var answersSeven = ["0", "0.5", "0.99999", "1"];
+var answersEight = ["1", "2", "1.1", "0",];
+var answersNine = ["var", "make", "let", "const"];
+var answersTen = ["style", "interact", "java", "script"];
 
 
 var clock = document.getElementById("countdown");
 
 var questionNumber = document.getElementById("qNumber");
 var questionContent = document.getElementById("qContent");
-
-
+var choiceList = document.getElementById("choices");
+var b1 = document.getElementById("1");
+var b2 = document.getElementById("2");
+var b3 = document.getElementById("3");
+var b4 = document.getElementById("4");
+console.log(b1.innerHTML);
+console.log(b2.innerHTML);
+console.log(b3.innerHTML);
+console.log(b4.innerHTML);
 
     var q = 1;
     var i = 0;
@@ -53,10 +47,25 @@ function nextQuestionAndAnswer() {
         questionNumber.innerHTML = "Question " + q;
         q++;
         questionContent.innerHTML = questions[i];
-        var randomButton = document.getElementById(Math.floor(1+(Math.random() * 4)));
-        randomButton.innerHTML = answers[i];
-        i++;
+};
+
+var array = [1, 2, 3, 4];
+// Following function 'shuffleArray' is a modification or the Fisher-Yates algorithm, sometimes attributed as the Durstenfeld algorithm. 
+// I used this function as there is no built in shuffle function for an array in Javascript.
+// Fisher Yates (Knuth) algorithm link = https://github.com/Daplie/knuth-shuffle
+// Durstenfeld algorithm reference (wikipedia) = https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+function shuffleArray() {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     };
+};
+shuffleArray();
+
+function choices () {
+
+}
+
 
 
 function countdown() {
